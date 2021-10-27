234d2d70d302976579930fea85a4e96eec0b0aeb
Ryan Normand <rnorman6@student.fitchburgstate.edu>
2021-10-27 17:04:50 -0400

    Implemented Win and Lose Conditions + Timer
    Added in countdown timer till end of game, as well as win or loss check

---
+++ Asteroid Crash - MGD - Ryan Normand/Assets/Scripts/Earth.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Earth : MonoBehaviour
+{
+    public float currentTime = 0f;
+
+    public float speed = 3f;
+
+    public GameObject Timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.up * speed * Time.deltaTime);
+        if (Timer.GetComponent<Timer>().currentTime == 10)
+        {
+            transform.Translate(Vector3.up * speed * Time.deltaTime);
+            Debug.Log("Upsy!");
+        }
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Log("Game Over!");
+        }
+    }
+}
---
+++ Asteroid Crash - MGD - Ryan Normand/Assets/Scripts/Timer.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public Text timerText;
+    public GameObject youloseText;
+    public GameObject youwinText;
+
+    private float startTime = 40f;
+    public float currentTime = 0f;
+    public float speed = 0.3f;
+
+    public static float size;
+
+    void Start()
+    {
+
+        currentTime = startTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime -= 1 * Time.deltaTime;
+        timerText.text = currentTime.ToString("0");
+
+        if(currentTime <= 0)
+        {
+            currentTime = 0;
+            timerText.color = Color.red;
+        }
+
+        if (currentTime <= 10)
+        {
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        }
+
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Time.timeScale = 0;
+            Debug.Log("Game Over!");
+
+            if (size >= 10)
+            {
+                youwinText.SetActive(true);
+            }
+
+            if (size < 10)
+            {
+                youloseText.SetActive(true);
+            }            
+        }
+    }
+}
